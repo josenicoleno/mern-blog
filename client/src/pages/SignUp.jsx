764b8d6ccadf86bd4a6a10241c5702edacb64254
Jose Nicoleno <josenicoleno@hotmail.com>
2024-10-15 22:21:47 +0200

    create sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Label, TextInput } from 'flowbite-react'
 
 const SignUp = () => {
   return (
-    <div>
-      Sign up
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>José Nicoleno's</span>
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>This is my blogpage. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod tenetur eum soluta. Nam animi blanditiis.</p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username' />
+              <TextInput id='username' type='text' placeholder='Username' />
+            </div>
+            <div>
+              <Label value='Your email' />
+              <TextInput id='email' type='text' placeholder='name@company.com' />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput id='password' type='password' placeholder='Password' />
+            </div>
+            <Button gradientDuoTone='purpleToPink'>Sign up</Button>
+          </form>
+          <div className="">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500'> Sign In</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
