d27e511744ecab5066c0e3689ec45b5fcce61234
Jose Nicoleno <josenicoleno@hotmail.com>
2024-10-16 23:50:18 +0200

    complete signin page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
