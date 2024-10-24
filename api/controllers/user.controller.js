cd1cd99befbf04e290ee89163619403ced15e112
Jose Nicoleno <josenicoleno@hotmail.com>
2024-10-24 13:56:18 +0200

    create get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -70,3 +70,43 @@ export const signout = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+  const startIndex = parseInt(req.query.startIndex || 0);
+  const limit = parseInt(req.query.limit || 9);
+  const sortDirection = req.query.sort === "asc" ? 1 : -1;
+  try {
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+    const totalUser = await User.countDocuments();
+
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const totalLastMonthUser = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUser,
+      totalLastMonthUser,
+    });
+  } catch (error) {
+    next(error)
+  }
+};
