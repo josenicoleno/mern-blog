9e9158310c4d8bfcdc567e38fc180bbbf77f9391
Jose Nicoleno <josenicoleno@hotmail.com>
2024-10-27 19:30:32 +0100

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold mx-auto p-3 text-center'>About José Nicoleno</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quis non labore similique nostrum placeat repellendus eius unde quas nam ducimus, saepe veniam rerum officia numquam vero dignissimos tempora fuga.</p>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores fugit delectus, deserunt error quisquam incidunt molestias praesentium expedita? Alias, omnis harum. Facilis aliquid, sapiente earum est temporibus sit similique placeat!</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit assumenda recusandae iusto laborum est quod incidunt eos numquam minima accusantium. Veritatis odit nostrum quidem ab, ut ea voluptatibus pariatur unde!</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction'
 
-const Projects = () => {
+export default function Projects() {
   return (
-    <div>
-      Projects
+    <div className='min-h-screen max-w-2xl mx-auto flex flex-col items-center justify-center gap-6'>
+      <h1 className='text-3xl font-semibold mx-auto p-3 text-center'>Projects</h1>
+      <p className='text-md text-gray-500 mx-auto p-3 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam repudiandae aliquid dolorem dolores quis vitae officiis aspernatur, assumenda laborum eveniet modi soluta doloremque tempore consequatur ullam quasi ab eius ducimus.</p>
+      <CallToAction />
     </div>
   )
 }
-
-export default Projects
