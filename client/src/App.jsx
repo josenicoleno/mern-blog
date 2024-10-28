4fcd59c4421c765dbf7b99ade89ec925376765bc
Jose Nicoleno <josenicoleno@hotmail.com>
2024-10-28 22:42:20 +0100

    add page not found

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import UpdatePost from './pages/UpdatePost'
 import Post from './pages/Post'
 import ScrollToTop from './components/ScrollToTop'
 import Search from './pages/Search'
+import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
         <Route path='/home' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
