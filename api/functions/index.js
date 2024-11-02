62e0755e74e7348ca5531599c71ab45e6057dcb5
Jose Nicoleno <josenicoleno@hotmail.com>
2024-11-02 17:50:53 +0100

    fix v6

--- api/functions/index.js
+++ api/functions/index.js
@@ -34,6 +34,7 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+app.use("/.netlify/functions/index/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/contact", contactRoutes);
