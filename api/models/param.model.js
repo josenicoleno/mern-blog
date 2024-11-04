b9c5ebd9ed5cd5e2e45ad178f61d142d1f9f9b1e
Jose Nicoleno <josenicoleno@hotmail.com>
2024-11-04 12:40:04 +0100

    create API and Model Params

---
+++ api/models/param.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const paramSchema = new mongoose.Schema(
+  {
+    key: { type: String, required: true },
+    text: { type: String},
+    date: { type: Date },
+    image: { type: String },
+    boolean: { type: Boolean },
+    number: { type: Number },
+  },
+  { timestamps: true }
+);
+
+const Param = mongoose.model("Param", paramSchema);
+
+export default Param;
+
--- api/routes/param.route.js
+++ api/routes/param.route.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { createParam } from "../controllers/param.controller.js";
+import { createParam, deleteParam, getParams, updateParam } from "../controllers/param.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
-router.post("/create", createParam);
+router.post("/create", verifyToken, createParam);
+router.get("/:key", getParams);
+router.put("/:id", verifyToken, updateParam);
+router.delete("/:id", verifyToken, deleteParam);
 
 export default router;
