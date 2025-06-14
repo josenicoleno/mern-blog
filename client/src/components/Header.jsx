638f4ad93ee7b05ef9b4f702309f79f0d2898f5d
Jose Nicoleno <josenicoleno@hotmail.com>
2025-06-14 17:18:37 +0200

    actualiza el componente Header para que el ícono de búsqueda sea un enlace y modifica la página About para usar un título dinámico y contenido HTML seguro

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -81,10 +81,12 @@ const Header = () => {
                     />
                 </form>
                 <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-                    <AiOutlineSearch />
+                    <Link to='/search'>
+                        <AiOutlineSearch />
+                    </Link>
                 </Button>
                 <div className="flex gap-2 md:order-2">
-                    <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
+                    <Button className='w-12 h-10 sm:inline' color='gray' pill onClick={() => dispatch(toggleTheme())}>
                         {theme === 'light' ? <FaMoon /> : <FaSun />}
                     </Button>
                     {currentUser ? (
