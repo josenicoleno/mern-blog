083c933e487cf96b11f364fa77415161e59540a6
Jose Nicoleno <josenicoleno@hotmail.com>
2026-05-09 21:20:50 +0200

    refactor: Actualiza la sección "Sobre Mí" con nueva información profesional y mejora el diseño de la interfaz

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { DiJavascript1, DiReact, DiNodejs, DiPython, DiDjango, DiHtml5, DiCss3, DiMongodb, DiPostgresql } from "react-icons/di";
+import { AiFillGoogleCircle, AiOutlineDownload, AiOutlineExport, AiOutlineOpenAI } from "react-icons/ai";
+import { DiJavascript1, DiReact, DiNodejs, DiPython, DiDjango, DiHtml5, DiCss3, DiMongodb, DiPostgresql, DiMsqlServer, DiJava, DiTerminal, DiWindows } from "react-icons/di";
 import { useLanguage } from '../contexts/LanguageContext';
 
 export default function About() {
@@ -15,7 +15,7 @@ export default function About() {
   const translations = {
     es: {
       title: "Sobre Mí",
-      summary: "Resumen",
+      summary: "Desarrollador de software con amplia experiencia en el desarrollo web de sistemas complejos y gestión de bases de datos. Especializado en tecnologías como GeneXus, ReactJS y Python, con sólidos conocimientos en metodologías ágiles como Scrum y prácticas DevOps. Capacidad para liderar equipos, para comunicarse con clientes con diferentes perfiles, diseñar soluciones escalables y optimizar procesos. Enfoque centrado en la eficiencia, la automatización y la mejora continua.",
       contact: "Datos de Contacto",
       technologies: "Tecnologías",
       languages: "Idiomas",
@@ -24,22 +24,22 @@ export default function About() {
       myResume: "Descargar CV",
       italianCV: "Scarica CV",
       job1: {
-        title: "Desarrollador Full Stack",
-        company: "Empresa XYZ",
-        dates: "Enero 2022 - Presente",
-        desc: "Desarrollo de aplicaciones web usando React y Node.js. Colaboración en equipos ágiles."
+        title: "Technical Leader",
+        company: "Capgemini",
+        dates: "Septiembre 2023 - Presente",
+        desc: "Gestiono equipos de Italia, India y España para un cliente multinacional en actividades diarias de mantenimiento de entornos de producción y no producción.\n Colaboro en la planificación de sprints y en la resolución de incidencias críticas. Asigno tareas a los equipos y realizo el seguimiento de las mismas. \nTrabajo utilizando tecnologías como shell scripting y Python para automatización de tareas; mantenimiento y actualización de bases de datos como MongoDB, PostgreSQL y Oracle; mantenimiento de servidores Linux y Windows; documentación técnica; despliegue de aplicaciones; prevención de incidencias a través de monitorización con herramientas como Dynatrace y análisis de logs."
       },
       job2: {
-        title: "Desarrollador Frontend",
-        company: "Empresa ABC",
-        dates: "Junio 2020 - Diciembre 2021",
-        desc: "Creación de interfaces de usuario responsivas con HTML, CSS y JavaScript."
+        title: "Analista-Desarrollador / FullStack Developer",
+        company: "Tecnologística Consultores",
+        dates: "Junio 2022 - Agosto 2023",
+        desc: "Trabajé como analista y desarrollador de un sistema de logística, realizando mantenimiento de la aplicación y creación de nuevas funcionalidades. Utilicé Genexus con C# para el desarrollo de la aplicación y SQL Server para las bases de datos. Estuve en un entorno ágil, colaborando con el equipo para planificar y ejecutar tareas, y asegurando la calidad del código mediante pruebas unitarias y revisiones de código. Hemos utilizado como metodologías ágil Scrum, participando en reuniones diarias, planificación de sprints y retrospectivas. Utilicé herramientas de gestión de proyectos para las asignaciones, seguimiento y control de tareas. Colaboré estrechamente con el equipo para garantizar la entrega de funcionalidades de alta calidad dentro de los plazos establecidos."
       },
       job3: {
-        title: "Pasante en Desarrollo",
-        company: "Empresa DEF",
-        dates: "Enero 2019 - Mayo 2020",
-        desc: "Aprendizaje de tecnologías backend y bases de datos."
+        title: "Project Manager y Analista funcional",
+        company: "Dirmod",
+        dates: "Noviembre 2021 - Mayo 2022",
+        desc: "Gestioné equipos de desarrollo, tuve reuniones con clientes para revisiones contractuales; hice reuniones de revisión de proyectos, planificación de tareas y asignación de las mismas. Realicé análisis funcionales para definir los requisitos del proyecto y asegurar que el equipo de desarrollo comprendiera claramente las necesidades del cliente. Hice uso de Azure DevOps para la gestión de proyectos, diseño con Figma para el diseño de pantallas y Draw.io para el diseño de diagramas de flujo y arquitectura del sistema. Trabajé en estrecha colaboración con el equipo de desarrollo para garantizar la entrega de soluciones de alta calidad que cumplieran con los requisitos del cliente dentro de los plazos establecidos."
       },
       techList: [
         { name: "JavaScript", icon: DiJavascript1 },
@@ -50,7 +50,9 @@ export default function About() {
         { name: "HTML5", icon: DiHtml5 },
         { name: "CSS3", icon: DiCss3 },
         { name: "MongoDB", icon: DiMongodb },
-        { name: "PostgreSQL", icon: DiPostgresql }
+        { name: "PostgreSQL", icon: DiPostgresql },
+        { name: "Linux", icon: DiTerminal },
+        { name: "Windows Server", icon: DiWindows }
       ],
       langList: [
         { flag: "🇪🇸", language: "Spanish", level: "Native" },
@@ -60,7 +62,7 @@ export default function About() {
     },
     en: {
       title: "About Me",
-      summary: "Summary",
+      summary: "Software developer with extensive experience in web development of complex systems and database management. Specialized in technologies such as GeneXus, ReactJS and Python, with solid knowledge in agile methodologies such as Scrum and DevOps practices. Ability to lead teams, communicate with clients with different profiles, design scalable solutions and optimize processes. Focus on efficiency, automation and continuous improvement.",
       contact: "Contact Information",
       technologies: "Technologies",
       languages: "Languages",
@@ -69,7 +71,7 @@ export default function About() {
       myResume: "Download CV",
       italianCV: "Scarica CV",
       job1: {
-        title: "Full Stack Developer",
+        title: "Technical Leader",
         company: "Company XYZ",
         dates: "January 2022 - Present",
         desc: "Development of web applications using React and Node.js. Collaboration in agile teams."
@@ -114,7 +116,7 @@ export default function About() {
       myResume: "My resume",
       italianCV: "Scarica CV",
       job1: {
-        title: "Full Stack Developer",
+        title: "Technical Leader",
         company: "Company XYZ",
         dates: "January 2022 - Present",
         desc: "Development of web applications using React and Node.js. Collaboration in agile teams."
@@ -133,14 +135,16 @@ export default function About() {
       },
       techList: [
         { name: "JavaScript", icon: DiJavascript1 },
+        { name: "HTML5", icon: DiHtml5 },
+        { name: "CSS3", icon: DiCss3 },
         { name: "React", icon: DiReact },
         { name: "Node.js", icon: DiNodejs },
         { name: "Python", icon: DiPython },
-        { name: "Django", icon: DiDjango },
-        { name: "HTML5", icon: DiHtml5 },
-        { name: "CSS3", icon: DiCss3 },
         { name: "MongoDB", icon: DiMongodb },
-        { name: "PostgreSQL", icon: DiPostgresql }
+        { name: "PostgreSQL", icon: DiPostgresql },
+        { name: "SQLServer", icon: DiMsqlServer },  
+        { name: "Oracle SQL", icon: DiJava },
+        { name: "Shell scripting", icon: DiTerminal }
       ],
       langList: [
         { flag: "🇪🇸", language: "Spanish", level: "Native" },
@@ -159,6 +163,7 @@ export default function About() {
       if (res.ok) {
         const data = await res.json();
         setAbout(data.text);
+        setAbout(translations.es.summary)
       }
     };
     const fetchAboutTitle = async () => {
@@ -187,14 +192,17 @@ export default function About() {
             <button className="w-52 bg-transparent border border-white text-white px-4 py-2 rounded hover:bg-white hover:text-black transition" onClick={() => { spanishResume ? window.open(spanishResume, "_blank") : console.log('CV en español no disponible') }}>
               🇪🇸
               Descargar CV
+              <AiOutlineDownload className="inline-block ml-2" />
             </button>
             <button className="w-52 bg-transparent border border-white text-white px-4 py-2 rounded hover:bg-white hover:text-black transition" onClick={() => { englishResume ? window.open(englishResume, "_blank") : console.log('CV in English not available') }}>
               🇬🇧
               Download CV
+              <AiOutlineDownload className="inline-block ml-2" />
             </button>
             <button className="w-52 bg-transparent border border-white text-white px-4 py-2 rounded hover:bg-white hover:text-black transition" onClick={() => { italianResume ? window.open(italianResume, "_blank") : console.log('CV in Italian not available') }}>
               🇮🇹
               Scarica CV
+              <AiOutlineDownload className="inline-block ml-2" />
             </button>
           </div>
         </div>
@@ -204,19 +212,21 @@ export default function About() {
         <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
           {/* // Resumen a la izquierda */}
           <div className='md:col-span-1'>
-            <h2 className='text-2xl font-semibold mb-4'>{t.summary}</h2>
             <div className='mb-6'>
               <h3 className='text-xl font-medium mb-2 text-teal-500 underline'>{t.contact}</h3>
               <ul className='space-y-2'>
-                <li><strong>Email:</strong> josenicoleno@hotmail.com</li>
-                <li><strong>Teléfono: </strong>+39 3428360088</li>
-                <li><strong>LinkedIn:</strong> linkedin.com/in/jose-nicoleno/</li>
-                <li><strong>GitHub:</strong> github.com/josenicoleno</li>
+                <li><strong>Email:</strong> josenicoleno@hotmail.com <a href="mailto:josenicoleno@hotmail.com"><AiOutlineExport className="inline-block ml-2" /></a></li>
+                {/* <li><strong>Teléfono: </strong>+39 3428360088</li> */}
+                <li><strong>LinkedIn:</strong> linkedin.com/in/jose-nicoleno
+                  <a href="https://linkedin.com/in/jose-nicoleno/" target="_blank" rel="noopener noreferrer"><AiOutlineExport className="inline-block ml-2" /></a>
+                </li>
+                <li><strong>GitHub:</strong> github.com/josenicoleno <a href="https://github.com/josenicoleno/" target="_blank" rel="noopener noreferrer"><AiOutlineExport className="inline-block ml-2" /></a></li>
               </ul>
             </div>
             <div className='mb-6'>
               <h3 className='text-xl font-medium mb-2 text-teal-500 underline'>{t.title}</h3>
-              <div dangerouslySetInnerHTML={{ __html: about }} />
+              {/* <div dangerouslySetInnerHTML={{ __html: about }} /> */}
+              {about}
             </div>
             <div className='mb-6'>
               <h3 className='text-xl font-medium mb-2 text-teal-500 underline'>{t.technologies}</h3>
